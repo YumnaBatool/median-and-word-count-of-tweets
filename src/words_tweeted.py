9c4fabcdea0abb5bd4bbb2b2403801ae57cf06a2
Yumna Batool <syeda_yumna27@hotmail.com>
2015-07-14 16:20:53 +0500

    Update words_tweeted.py

--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,20 +1,19 @@
-f = open("tweet_input/tweets.txt", "r")
-tweet = f.read()
-words = tweet.split()
-WordFrequency = {}
+f = open("tweet_input/tweets.txt", "r")                             #opening tweets.txt file
+tweet = f.read()                                                    #reading tweets from the text file
+words = tweet.split()                                               #spliting tweet words by white space
+WordFrequency = {}                                                  #initializing dict to store word frequencies
 
-for word in words:
-    word = word.lower()
-    if word in WordFrequency.keys():
-        WordFrequency[word] += 1
-    else:
-        WordFrequency[word] = 1
+for word in words:                                                  #loop for each word in text file
+    if word in WordFrequency.keys():                                #if word is present in keys of dict
+        WordFrequency[word] += 1                                    #increment its value by 1
+    else:                                                           #if word is not present in keys of dict 
+        WordFrequency[word] = 1                                     #place it in dict as key with value = 1
 
-keylist = WordFrequency.keys()
-keylist.sort()
+keylist = WordFrequency.keys()                                      #saving words from dict keys in a keylist
+keylist.sort()                                                      #sorting dict keys alphabetically
 
-fname = "tweet_output/ft1.txt"
-f = open(fname, 'w')
-for key in keylist:
-    f.write(key + ": " + str(WordFrequency[key]) + "\n")
-f.close()
+fname = "tweet_output/ft1.txt"                                      #defining name of output file
+f = open(fname, 'w')                                                #opening output file
+for key in keylist:                                                 #loop for every word in keylist
+    f.write(key + ": " + str(WordFrequency[key]) + "\n")            #writing each word and its frequency in single line
+f.close()                                                           #closing the file
